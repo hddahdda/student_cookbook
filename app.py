3360ad06ec211601ca3c827ff5b3e59fc7a6660d
hddahdda <hedda9@hotmail.com>
2021-07-09 20:21:21 +0000

    created landing page index.html

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
+def home():
+    return render_template("index.html")
+
+
 @app.route("/get_recipes")
 def get_recipes():
     recipes = mongo.db.recipes.find()
