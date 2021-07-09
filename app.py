5a6c7a3675a94811e45dcc842b62690b899f0964
hddahdda <hedda9@hotmail.com>
2021-07-09 11:17:36 +0000

    removed 'int' fromapp.py file

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def get_recipes():
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+            port=os.environ.get("PORT")),
             debug=True)
