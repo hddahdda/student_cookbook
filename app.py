0c9afd652f19a48724dea2b88be98b1abf768998
hddahdda <hedda9@hotmail.com>
2021-07-13 11:28:46 +0000

    redirect to profile page from register route in app.py

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ def register():
         session["user"] = request.form.get("username").lower()
         """flash("You are now registred")
         """
+        return render_template("profile.html")
     return render_template("register.html")
 
 
@@ -61,6 +62,8 @@ def login():
             if check_password_hash(
                 username_exist["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
 
             else:
                 return redirect(url_for("login"))
@@ -69,6 +72,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
